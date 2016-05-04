8644c28a0c7853b91d52fed6c9a6e12d3525750f
Nicholas Chow <nickchow.stretch@gmail.com>
2016-05-03 23:01:32 -0700

    Add Pearson Correlation Coefficient

--- CollabFiltering/recommender.py
+++ CollabFiltering/recommender.py
@@ -1,5 +1,6 @@
 import pandas
 import numpy
+import math
 
 class Recommender(object):
 
@@ -18,7 +19,7 @@ class Recommender(object):
         # Precompute everyone's average votes.
         self._precomputeAvgVotes()
 
-        # Go down each row in the test data, predict the rating for each row.
+        # Iterate over each row in the test data, predict the rating for each row.
         for i in range(0, len(testD)):
             row = testD.iloc[[i]]
 
@@ -41,7 +42,7 @@ class Recommender(object):
     # Predicts the vote of user A on Item j.
     def _predictVote(self, user, item):
         # Compute the average vote for the active user.
-        V_a = self.avgVotes[user]
+        Vbar_a = self.avgVotes[user]
 
         nonZeroWeights = _computePearsonCoefficients(user)
         k = _computeNormalizer(nonZeroWeights)
@@ -51,14 +52,48 @@ class Recommender(object):
             V_ij = self._getVote(i, item)
             weightedNN += nonZeroWeights[i] * (V_ij - self.avgVotes[i])
 
-        return V_a + k*weightedNN
+        return Vbar_a + k*weightedNN
 
 
     # Returns a dictionary whose key consists of users against which
     # userA has a nonzero pearson correlation coefficient.
     # The value is the pearson correlation coefficient.
     def _computePearsonCoefficients(self, userA):
-        pass
+        # Grab every userId except for userA
+        allIds = self.trainData['userId'].unique()
+        otherUserIds = numpy.delete(allIds, numpy.where(allIds == userA))
+
+        # Dataframe slice for userA
+        userAData = self.trainData[self.trainData['userId'] == userA]
+
+        # Get active user's average vote
+        Vbar_a = self.avgVotes[userA]
+
+        # Compute Pearson Coefficients
+        for user in otherUserIds:
+            #Dataframe slice for userB
+            userBData = self.trainData[self.trainData['userId' == user]]
+            intersection = pandas.merge(
+                userAData, userBData, how='inner', on=['movieId'], suffixes=('_a', '_b'))
+
+            # add column that represents (V_aj - Vbar_a)
+            intersection['rating_adjusted_a']  = intersection['rating_a'].subtract(Vbar_a)
+
+            # add column that represents (V_ij - Vbar_i)
+            Vbar_i = self.avgVotes[user]
+            intersection['rating_adjusted_b'] = intersection['rating_b'].subtract(Vbar_i)
+
+            intersection['numeratorProduct'] = intersection.apply(
+                lambda row: row['rating_adjusted_a'] * row['rating_adjusted_b'],
+                axis=1,
+                raw=True)
+
+            numerator = intersection['numeratorProduct'].sum()
+            denominator = math.sqrt(
+                (intersection['rating_adjusted_a']**2).sum() *
+                (intersection['rating_adjusted_b']**2).sum())
+
+            r =  numerator/denominator
 
 
     def _computeMAE(self):
@@ -73,26 +108,18 @@ class Recommender(object):
         pass
 
 
-    # Given a user, computes their average vote.
-    def _computeAvgVote(self, user):
-
-        pass
-
-
-    # Retrieves the set of items on which user has voted.
-    def _getVotedSet(self, user):
-        pass
-
-
     # Helper to get the vote for user i on item j.
-    def _getVote(self, i, j):
+    def _getVote(self, user_i, item_j):
         return self.trainData[
-            (self.trainData['userId'] == i) &
-            (self.trainData['movieId'] == item)]['rating']
+            (self.trainData['userId'] == user_i) &
+            (self.trainData['movieId'] == item_j)]['rating']
 
 
     # Precomputes the average vote for every user in the
     # training set and stores it as an instance variable.
     def _precomputeAvgVotes(self):
-        # self.avgVotes =
-        pass
+        distinctUserIds = self.trainData['userId'].unique()
+
+        for userId in distinctUserIds:
+            ratings = self.trainData[trainData['userId'] == userId]['rating']
+            self.avgVotes[userId] = ratings.sum()/len(ratings)
